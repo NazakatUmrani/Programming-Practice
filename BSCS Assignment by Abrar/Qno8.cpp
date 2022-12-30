aefd7d42e0ef7bb2c313095f63178e114b9d314e
NazakatUmrani <unazakat70@gmail.com>
2022-12-30 20:10:32 +0500

    Abrar Assignment 1 Question was empty file and other was remaining

--- BSCS Assignment by Abrar/Qno8.cpp
+++ BSCS Assignment by Abrar/Qno8.cpp
@@ -5,16 +5,17 @@ using namespace std;
 
 int main()
 {
-    int x, user_input;
-    int ten_natural_numbers;
+    int user_input;
+    int ten_natural_numbers, increment;
 
     cout<<"Enter a number, and we will add nine increasing natural numbers to it: ";
     cin>>user_input;
 
-    while (x == 10)
-    {
-        /* code */
+    increment = user_input+1;
+    for(int i=0; i<9; i++){
+        user_input += increment;
+        increment++;
     }
-    
+    cout << "Result = " << user_input << endl;
     return 0;
 }
