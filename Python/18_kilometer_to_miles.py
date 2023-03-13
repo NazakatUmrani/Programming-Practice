7cbb68e559831e710abb13eb1361611c7f39ce6d
NazakatUmrani <unazakat70@gmail.com>
2023-03-13 20:52:09 +0500

    Python Km to miles, string operations

---
+++ Python/18_kilometer_to_miles.py
@@ -0,0 +1,3 @@
+distanceKM = int(input("Enter distance in Kilometers: "))
+distanceM = distanceKM / 1.609344
+print(f'{distanceKM}Km = {distanceM}M')
---
+++ Python/19_string_operations.py
@@ -0,0 +1,12 @@
+message = "Hello how are you, My Name is Nazakat Umrani"
+print(message.lower())
+print(message)
+print(message.upper())
+print(message.center(50,"-"))
+print(message.ljust(50,"-"))
+print(message.rjust(50,"-"))
+print(message.capitalize())
+print(message.count('a'))
+print(message.endswith('8'))
+
+
