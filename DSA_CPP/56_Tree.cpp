cfe77f2d5575e319b10a4ff7374b281f55a5f6fd
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-05-29 17:01:00 +0500

    Tree DS Added

---
+++ DSA_CPP/56_Tree.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <class T>
+class Node{
+    public:
+        Node<T> *left, *right;
+        T data;
+        Node(){
+            left=nullptr;
+            right=nullptr;
+        }
+        Node(T data){
+            this->data = data;
+            left= nullptr;
+            right= nullptr;
+        }
+};
+template <class T>
+class Tree{
+    public:
+    Node<T> *root;
+    Tree(){
+        root=nullptr;
+    }
+    void CreateTree(){
+        Node<T> *p = new Node<T>, *t = new Node<T>; //To hold front nodes in queue
+        queue<Node<T> *>q; //Store old nodes to add childs
+        int x;
+        cout << "Enter root data: " << flush;
+        cin >> x;
+        root=new Node<T>(x);
+        q.emplace(root);
+
+        while (! q.empty()){
+            p = q.front();
+            q.pop();
+
+            cout << "Enter left child data of " << p->data << ": " << flush;
+            cin >> x;
+            if (x != -1){
+                t = new Node<T>;
+                t->data = x;
+                t->left = nullptr;
+                t->right = nullptr;
+                p->left = t;
+                q.emplace(t);
+            }
+
+            cout << "Enter right child data of " << p->data << ": " << flush;
+            cin >> x;
+            if (x != -1){
+                t = new Node<T>;
+                t->data = x;
+                t->left = nullptr;
+                t->right = nullptr;
+                p->right = t;
+                q.emplace(t);
+            }
+        }
+    }
+    ~Tree() {
+        deleteTree(root);
+    }
+    void deleteTree(Node<T>* node) {
+    if (node == nullptr) return;
+
+    /* first delete both subtrees */
+    deleteTree(node->left);
+    deleteTree(node->right);
+
+    /* then delete the node */
+    delete node;
+}
+
+    void Preorder() { Preorder(root); }
+    void Inorder() { Inorder(root); }
+    void Postorder() { Postorder(root); }
+
+    void Preorder(Node<T> *p) {
+        if (p){
+            cout << p->data << ", " << flush;
+            Preorder(p->left);
+            Preorder(p->right);
+        }
+    }
+    void Inorder(Node<T> *p) {
+        if (p){
+            Inorder(p->left);
+            cout << p->data << ", " << flush;
+            Inorder(p->right);
+        }
+    }
+    void Postorder(Node<T> *p) {
+        if (p){
+            Postorder(p->left);
+            Postorder(p->right);
+            cout << p->data << ", " << flush;
+        }
+    }
+};
+
+int main(){
+
+    Tree<int> bt;
+
+    bt.CreateTree();
+    cout << endl;
+
+    cout << "Preorder: " << flush;
+    bt.Preorder();
+    cout << endl;
+
+    cout << "Inorder: " << flush;
+    bt.Inorder();
+    cout << endl;
+
+    cout << "Postorder: " << flush;
+    bt.Postorder();
+    cout << endl;
+
+    return 0;
+}
